f2a378be1d6839cf2bc46dff8d39bb852a55a3ae
Prasanna Yalala <yprasannakumar@gmail.com>
2018-04-06 08:56:46 +0100

    updated aemcomponent.js

--- jcr_root/etc/designs/aemcm/clientlib-aemcomponent/js/aemcomponent.js
+++ jcr_root/etc/designs/aemcm/clientlib-aemcomponent/js/aemcomponent.js
@@ -1,21 +1,56 @@
+"use strict";
+/**
+ * @function AEMComponent
+ * @description consoles out a name
+ * 
+ * @author ypk
+ * @version 1.1
+ * @returns {object} - contains a method to initiate the function
+ */
+
 function AEMComponent() {
 
-  var functionProps, $component, defaultProps = {
+  /* global properties */
+  var functionProps;
+
+  // default properties
+  var defaultProps = {
     functionName: "AEMComponent"
   };
 
-  var aemcomponent = function() {
-    // Add functionality here
-    console.log($component);
+  /**
+   * @function log
+   * @description consoles a string from a property
+   */
+
+  var log = function() {
+
+    console.log({
+      "Component Name": functionProps.functionName,
+      "Property": functionProps.customProperty
+    })
+
   };
+
+  /**
+   * @function init
+   * @description kickstarts the code
+   * 
+   * @param {object} properties - JSON object passed in through the config
+   */
 
   var init = function(properties) {
-    functionProps = Object.assign(defaultProps, properties);
-    $component = $(document.getElementById(functionProps.componentId));
-    aemcomponent();
-  };
 
+    // extend global properties with default properties
+    functionProps = Object.assign(defaultProps, properties);
+
+    // calling private function
+    log();
+    
+  };
+  
   return {
     init: init
   };
+  
 }
